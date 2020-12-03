385247b2dee94677896d8056ea978d290b86cda1
77Michaux <michaux@et.esiea.fr>
2020-12-03 19:49:33 +0100

    Test_1

---
+++ site/inc/bas.inc.php
@@ -0,0 +1,9 @@
+            </div>
+        </section>
+        <footer>
+            <div class="conteneur">    
+                <?php echo date('Y'); ?> - Tous droits reservés - MICHAUX Paul 2020.
+            </div>
+        </footer>
+    </body>
+</html>
